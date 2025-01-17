41e260c84a03d11304d47c48b5c6e22060f0baf7
PureSlurp <sraymor17@gmail.com>
2025-01-16 21:38:39 -0500

    removed unused

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,5 @@
 // src/components/layout/Layout.tsx
 import { ReactNode } from 'react';
-import Navbar from './Navbar';
-import Footer from './Footer';
 
 interface LayoutProps {
   children: ReactNode;
